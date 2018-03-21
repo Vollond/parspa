e84316102d7b0a3314677de4f45832ad42215271
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-21 20:35:41 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,8 @@
 <?php
+global $db;
 $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
+$databasename = "heroku_6da0db36e924815";
 
 mysql_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
 
@@ -8,4 +10,3 @@ mysql_query('SET NAMES utf8',$db);
 mysql_query('SET CHARACTER SET utf8',$db );
 mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
 setlocale(LC_ALL,"ru_RU.UTF8");
-echo "true";
