eeb831c6bad4f65ba1c484c1cf0b8ae0d9f702ea
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-21 20:31:22 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -1,17 +1,6 @@
 <?php
 $db = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
-echo $db["port"];
-echo "  \n";
-echo $db["host"];
-echo "  \n";
-echo $db["path"];
-echo "  \n";
-echo $db["user"];
-
-
-global $db;
-$db = mysql_connect($host,$username,$password) or die("error: Failed_connect_database");
 
 mysql_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
 
@@ -19,3 +8,4 @@ mysql_query('SET NAMES utf8',$db);
 mysql_query('SET CHARACTER SET utf8',$db );
 mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
 setlocale(LC_ALL,"ru_RU.UTF8");
+echo "true";
