bba28836abcf7b6802158cb6a348d5534c3a1fd4
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-21 20:42:44 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,13 @@
 <?php
-global $db;
-$db = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$db["path"] = ltrim($db["path"], "/");
+/*подключение к базе данных*/
+
+$host = "eu-cdbr-west-02.cleardb.net"; // в 90% случаев это менять не надо
+$password = "faf87cca";
+$username = "bd8f9d12dfabef";
 $databasename = "heroku_6da0db36e924815";
+
+global $db;
+$db = mysql_connect($host,$username,$password) or die("error: Failed_connect_database");
 
 mysql_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
 
