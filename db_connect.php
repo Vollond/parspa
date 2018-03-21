9eaf41aad984c436e3644ee6fd98fb4b34f39dce
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-21 15:25:50 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,10 @@
 <?php
 /*подключение к базе данных*/
 
-$host = "localhost"; // в 90% случаев это менять не надо
-$password = "пароль";
-$username = "имя юзера";
-$databasename = "имя базы";
+$host = "ec2-54-228-181-43.eu-west-1.compute.amazonaws.com"; // в 90% случаев это менять не надо
+$password = "a26cd18c3d7dcc49c094c6a53c539d6d8e040eec033ce6086a3624fc35d0765a";
+$username = "jprgrwfmiyzodr";
+$databasename = "deni4gm2l8i6b7";
 
 global $db;
 $db = mysql_connect($host,$username,$password) or die("error: Failed_connect_database");
