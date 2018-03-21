8fec7db36883ff1bdaaaf56101a79222272f2e76
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-21 17:27:48 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -1,32 +1,12 @@
 <?php
 echo "123";
 echo getenv('DATABASE_URL');
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
+$db = parse_url(getenv("DATABASE_URL"));
+$db["path"] = ltrim($db["path"], "/");
 
-$app->get('/db/', function() use($app) {
-  $st = $app['pdo']->prepare('SELECT name FROM test_table');
-  $st->execute();
 
-  $names = array();
-  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-    $app['monolog']->addDebug('Row ' . $row['name']);
-    $names[] = $row;
-  }
-
-  return $app['twig']->render('database.twig', array(
-    'names' => $names
-  ));
-});
-echo "\n321";
+	$query = "select 'Привет!' as field_1, 123 as field_2";
+	$result = pg_query($db, $query);
+	$result = pg_fetch_assoc($result); 
+	echo $result['field_1'] . '</br>' . $result['field_2'];
+        pg_close($db);
