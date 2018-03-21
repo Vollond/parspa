0b8aa312dc817f2a30acc740b5d2cacbbeb1314e
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-21 20:45:32 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -5,12 +5,12 @@ $host = "eu-cdbr-west-02.cleardb.net"; // в 90% случаев это меня�
 $password = "faf87cca";
 $username = "bd8f9d12dfabef";
 $databasename = "heroku_6da0db36e924815";
-
+echo "1";
 global $db;
 $db = mysql_connect($host,$username,$password) or die("error: Failed_connect_database");
-
+echo "2";
 mysql_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
-
+echo "3";
 mysql_query('SET NAMES utf8',$db);
 mysql_query('SET CHARACTER SET utf8',$db );
 mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
