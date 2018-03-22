b5cc99993e4bc33b8a7f1f48f042f2272e34be79
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-22 12:59:07 +0200

    1

--- db_connect.php
+++ db_connect.php
@@ -5,13 +5,13 @@ $host = "eu-cdbr-west-02.cleardb.net:3306"; // в 90% случаев это ме
 $password = "faf87cca";
 $username = "bd8f9d12dfabef";
 $databasename = "heroku_6da0db36e924815";
-phpinfo();
 global $db;
+echo "2";
 $db = mysqli_connect($host,$username,$password);
 echo "2";
-mysql_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
+mysqli_connect($databasename, $db) or die("error:Database not selected witch mysql_select_db");
 echo "3";
-mysql_query('SET NAMES utf8',$db);
-mysql_query('SET CHARACTER SET utf8',$db );
-mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
+mysqli_connect('SET NAMES utf8',$db);
+mysqli_connect('SET CHARACTER SET utf8',$db );
+mysqli_connect('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
 setlocale(LC_ALL,"ru_RU.UTF8");
