9276c3d46b9e78dea435cd397d2111b7908a1625
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-22 18:41:03 +0200

    ё

--- users.php
+++ users.php
@@ -4,7 +4,7 @@
 require_once("db_connect.php");
 global $db;
 $name = "123";
-$result = mysqli_query("select * from users where name = '{$name}' LIMIT 1",$db);
+$result = mysqli_query("select from users where name = '{$name}' LIMIT 1",$db);
 echo "$result";
 /*
 if(mysqli_fetch_array($result) !== false) echo "не фолс";
