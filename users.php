d4ed265be5412d1793fcb049be4adca05de1d769
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-22 15:47:37 +0200

    1

--- users.php
+++ users.php
@@ -16,7 +16,7 @@ echo $mysqli_error;
 */
 function make_user($name){
 	global $db;
-	$name = mysqli_real_escape_string($name);
+//	$name = mysqli_real_escape_string($name);
 //	$chat_id = mysqli_real_escape_string($chat_id);
 	$query = "insert into users (name) values('{$name}')";
 	mysqli_query($db,$query) or die("пользователя создать не удалось");
