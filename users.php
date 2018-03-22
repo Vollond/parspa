7033b5b277640f2f170cdf6fa1c49c6c200995c2
Vollond <34167687+Vollond@users.noreply.github.com>
2018-03-22 13:08:36 +0200

    1

--- users.php
+++ users.php
@@ -3,39 +3,39 @@
 
 function make_user($name,$chat_id){
 	global $db;
-	$name = mysql_real_escape_string($name);
-	$chat_id = mysql_real_escape_string($chat_id);
+	$name = mysqli_real_escape_string($name);
+	$chat_id = mysqli_real_escape_string($chat_id);
 	$query = "insert into `users`(name,chat_id) values('{$name}','{$chat_id}')";
-	mysql_query($query,$db) or die("пользователя создать не удалось");
+	mysqli_query($query,$db) or die("пользователя создать не удалось");
 }
 
 function is_user_set($name){
 	global $db;
-	$name = mysql_real_escape_string($name);
-	$result = mysql_query("select * from `users` where name='$name' LIMIT 1",$db);
+	$name = mysqli_real_escape_string($name);
+	$result = mysqli_query("select * from `users` where name='$name' LIMIT 1",$db);
 
-    if(mysql_fetch_array($result) !== false) return true;
+    if(mysqli_fetch_array($result) !== false) return true;
     return false;
 }
 
 // задание настройки
 function set_udata($name,$data = array()){
 	global $db;
-	$name = mysql_real_escape_string($name);
+	$name = mysqli_real_escape_string($name);
 	if(!is_user_set($name)){
 		make_user($name,0); // если каким-то чудом этот пользователь не зарегистрирован в базе
 	}
 	$data = json_encode($data,JSON_UNESCAPED_UNICODE);
-	mysql_query("update `users` SET data_json = '{$data}' WHERE name = '{$name}'",$db); // обновляем запись в базе
+	mysqli_query("update `users` SET data_json = '{$data}' WHERE name = '{$name}'",$db); // обновляем запись в базе
 }
 
 // считываение настройки
 function get_udata($name){
 	global $db;
 	$res = array();
-	$name = mysql_real_escape_string($name);
-	$result = mysql_query("select * from `users` where name='$name'",$db);
-	$arr = mysql_fetch_assoc($result);
+	$name = mysqli_real_escape_string($name);
+	$result = mysqli_query("select * from `users` where name='$name'",$db);
+	$arr = mysqli_fetch_assoc($result);
     if(isset($arr['data_json'])){
 		$res = json_decode($arr['data_json'], true);
 	}
