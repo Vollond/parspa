ef1827c3a8d8b1a9341c2da8d8772d544dcee431
Vollond <34167687+Vollond@users.noreply.github.com>
2018-05-15 13:09:28 +0300

    1

--- pars.php
+++ pars.php
@@ -12,18 +12,30 @@ $html = file_get_contents("http://web.kpi.kharkov.ua/cmps/ru/category/glavnaya/"
 phpQuery::newDocument($html);
 
 $links = pq("#post-3372")->find("a");
-
+$imgs pq("#post-3372")->find("img");
 $tmp = array();
 
 foreach($links as $link){
 
 	$link = pq($link);
 
+
 	$tmp[] = array(
 		"text" => $link->text(),
 		"url"  => $link->attr("href")
 	);
 }
+
+foreach($imgs as $img){
+
+	$img = pq($img);
+
+
+	$tmp[] = array(
+		"img"  => $img->attr("src")
+	);
+}
+	$img = pq($img);
 
 phpQuery::unloadDocuments();
 ?>
@@ -33,6 +45,7 @@ phpQuery::unloadDocuments();
 	<li>
 		<a href="<?php echo($value["url"]); ?>" target="_blank">
 			<?php echo($value["text"]); ?>
+			<?php echo($value["img"]); ?>
 		</a>
 	</li>
 	<?php endforeach; ?>
