8ab12feea3aaff0aa65c78b4ea7f913e3f3741b8
Vollond <34167687+Vollond@users.noreply.github.com>
2018-05-15 12:49:56 +0300

    1

--- pars.php
+++ pars.php
@@ -5,4 +5,35 @@
  * @author - Александр Штокман
  */
 header('Content-Type: text/html; charset=utf-8');
-echo file_get_contents('http://web.kpi.kharkov.ua/cmps/ru/category/glavnaya/');
+require('PQ/phpQuery/phpQuery.php');
+
+$html = file_get_contents("https://yandex.by/");
+
+phpQuery::newDocument($html);
+
+$links = pq("#tabnews_newsc")->find("a");
+
+$tmp = array();
+
+foreach($links as $link){
+
+	$link = pq($link);
+
+	$tmp[] = array(
+		"text" => $link->text(),
+		"url"  => $link->attr("href")
+	);
+}
+
+phpQuery::unloadDocuments();
+?>
+
+<ul>
+	<?php foreach($tmp as $value): ?>
+	<li>
+		<a href="<?php echo($value["url"]); ?>" target="_blank">
+			<?php echo($value["text"]); ?>
+		</a>
+	</li>
+	<?php endforeach; ?>
+</ul>
