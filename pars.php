9f95d9dd8d3a9352e562f1b02e099507ff85b7a2
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-08 21:54:31 +0300

    1

--- pars.php
+++ pars.php
@@ -26,7 +26,7 @@ foreach($links as $link){
 	$tmp[] = array(
 		"text" => $link->text(),
 		"url"  => $link->attr("href"),
-		"img"  => $link->find("img")->find("scr"),
+		"img"  => $link->find("img")->attr("scr"),
 		"text2" => $link->find("p")
 		
 	);
