1c26dba9e52c7da3cca64513f113827d0c65d09a
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-09 22:27:04 +0300

    1

--- pars.php
+++ pars.php
@@ -75,7 +75,7 @@ foreach($tmp as $value):
 		$url = substr( $url, 6);
 		
 		$img2 = preg_replace('/(min-|)\d\d\dx\d\d\d\./', '.', $img);
-		echo "\n $img2 \n"
+		echo "\n $img2 \n";
 		echo"$txt \n $img \n  $url ";
 	
 }
