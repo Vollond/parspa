cac6f31c91b8ac0ece059671ebc6db02726494e5
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-09 19:13:34 +0300

    1

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ require_once("vendor/autoload.php");
 // подрубаем базу данных
 require_once("db_connect.php");
 require_once("users.php");
-require_once("pars.php");
 
 
 // дебаг
@@ -58,17 +57,104 @@ $bot->command('start', function ($message) use ($bot) {
     $bot->sendMessage($message->getChat()->getId(), $answer);
 	make_user($message->getFrom()->getUsername(),$cid);
 });
-/*
+
 $bot->command('update_posts', function ($message) use ($bot) {
-	$img = "http://web.kpi.kharkov.ua/cmps/wp-content/uploads/sites/144/2018/03/28828314_1837764673183610_8045670836477995835_o-min-225x150.jpg";
+	/*$img = "http://web.kpi.kharkov.ua/cmps/wp-content/uploads/sites/144/2018/03/28828314_1837764673183610_8045670836477995835_o-min-225x150.jpg";
 	$plink = "http://web.kpi.kharkov.ua/cmps/ru/kharkiv-project-management-day-krupnejshee-it-sobytie-v-ukraine/";
 	$ptext = "10 марта состоялась конференция, посвященная проектному менеджменту в IT — Kharkiv Project Management Day — крупнейшее событие в Украине. В конференции приняло участие около 400 проектных IT-менеджеров со всей страны. Своим опытом для них поделились более 40 опытных спикеров, среди них доцент кафедры компьютерного моделирования процессов и систем, к.т.н. — … ";
 	$p_text = "$ptext [Читать дальше]($plink)";
 	$bot->sendPhoto("@kaftest", $img);
 	$bot->sendMessage("@kaftest", $p_text, "markdown");
+*/
+	$bot->sendMessage("@kaftest", "123");
+$html = file_get_contents("http://web.kpi.kharkov.ua/cmps/ru/category/glavnaya/");
+$pq = phpQuery::newDocument($html);
 
+
+$articls = $pq->find("article");
+
+
+$art = array();
+
+foreach($articls as $article){
 	
-});*/
+$article = pq($article);
+	$art[] = array(
+	"num" => $article->attr("id")
+	);
+}
+
+
+phpQuery::unloadDocuments();
+
+foreach($art as $value): 
+echo ($value["num"]);
+add_post($value["num"]);
+echo "\n";
+endforeach;
+});
+
+
+
+
+function pars_post($post_id){
+$html = file_get_contents("http://web.kpi.kharkov.ua/cmps/ru/category/glavnaya/");
+
+$pq = phpQuery::newDocument($html);
+
+$links = pq("#$post_id")->find("div");
+
+$tmp = array();
+
+foreach($links as $link){
+
+	$link = pq($link);
+
+	$tmp[] = array(
+		"text" => $link->text(),
+		"url"  => $link->attr("href"),
+		"img"  => $link->find("img"),
+		"text2" => $link->find("p")
+	);
+}
+
+phpQuery::unloadDocuments();
+
+
+foreach($tmp as $value): 
+		preg_match_all('/(img|src)=("|\')[^"\'>]+/i',$value["img"], $result);
+		preg_match_all('/(href)=("|\')[^"\'>]+/',$value["text2"], $result2);
+		preg_match_all('/(<p>)[^<]+/',$value["text2"], $result3);
+		$txt = $result3[0][0];
+		$img = $result[0][0];
+		$url = $result2[0][0];
+	 endforeach; 
+
+		$txt = substr( $txt, 3);
+		$img = substr( $img, 5);
+		$url = substr( $url, 6);
+		echo"$txt \n $img \n  $url ";
+		
+	$p_text = "$txt [Читать дальше]($url)";
+	$bot->sendPhoto("@kaftest", $img);
+	$bot->sendMessage("@kaftest", $p_text, "markdown");
+
+}
+
+
+function add_post($num){
+	global $db;
+	$query = "insert into posts (num) values('{$num}')";
+	if (mysqli_query($db,$query)==true)
+	{
+		echo "true!!";
+		pars_post($num);
+}
+}
+
+
+
+
 
 // помощ
 $bot->command('help', function ($message) use ($bot) {
