d0faf99570f980eea0d7c248b2574b8fb1e55fd2
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-10 13:47:26 +0300

    1

--- index.php
+++ index.php
@@ -86,7 +86,7 @@ $bot->on(function($update) use ($bot, $callback_loc, $find_command){
 
 	if($data == "data_test"){
 		//$love1="$text 1";
-			$like = (
+			$like = \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
 		[
 			[
 				['callback_data' => 'data_test', 'text' => "love1"],
