966fbba925ec65cd6d2538495c564999d59ca784
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-10 21:27:46 +0300

    1

--- index.php
+++ index.php
@@ -512,7 +512,7 @@ e-mail: cmpskhpi@gmail.com
 	[Группа в вк](https://vk.com/official_cmps)
 	[Канал в телеграме](https://t.me/official_cmps)
 ";
-		$bot->sendMessage($message->getChat()->getId(), $k_txt3, "markdown", false);	
+		$bot->sendMessage($message->getChat()->getId(), $k_txt3, "markdown", false, null, null, null);	
 		}
 	
 	
