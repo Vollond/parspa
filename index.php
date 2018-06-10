32bcb28fd358ec320ef72d84771bdde9429ef27d
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-10 15:54:20 +0300

    1

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ $bot->on(function($update) use ($bot, $callback_loc, $find_command){
 		//$love1="$text 1";
 			 //$callback->getId()
 
-		$bot->editMessageReplyMarkup($chatId, $message->getMessageId(),null, $arrayll);
+		$bot->editMessageReplyMarkup($chatId, $message->getMessageId(),null, $like2);
 		}
 	//	$bot->editMessageReplyMarkup($chatId,$message, $inlmsgid,$like2);
 	//	$bot->answerCallbackQuery( $callback->getId(), "This is Ansver!",true);	}
@@ -293,7 +293,10 @@ $bot->on(function($update) use ($bot, $callback_loc, $find_command){
 	$data = $callback->getData();
 	
 	if($data == "data_test"){
+		$bot->editMessageReplyMarkup($chatId, $message->getMessageId(),null, $like2);
+		
 		$bot->answerCallbackQuery( $callback->getId(), "This is Ansver!",true);
+		
 	}
 	if($data == "data_test2"){
 		$bot->sendMessage($chatId, "Это ответ!");
