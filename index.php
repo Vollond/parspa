b8305846a57d2fd2e32ad43d0555b5c0e268c599
Vollond <34167687+Vollond@users.noreply.github.com>
2018-06-19 12:56:47 +0300

    1

--- index.php
+++ index.php
@@ -477,13 +477,13 @@ $bot->on(function($Update) use ($bot){
 -«Проектирование, создание и анализ компьютерных систем»
 [Информация для абитуриентов](http://web.kpi.kharkov.ua/cmps/ru/abiturientam/)
 ";
-
-		$bot->sendMessage($message->getChat()->getId(), $k_txt, "markdown");	
-		$bot->sendMessage($message->getChat()->getId(), $k_txt2, "markdown");	
 		$bot->sendPhoto($message->getChat()->getId(), $k_img);
 		$bot->sendDocument($message->getChat()->getId(), $k_img);
 		$bot->sendDocument($message->getChat()->getId(), "http://web.kpi.kharkov.ua/cmps/wp-content/uploads/sites/144/2016/02/Animatsiya-na-sajt-menshe-8-mb.gif");
 		$bot->sendVideo($message->getChat()->getId(), "http://web.kpi.kharkov.ua/cmps/wp-content/uploads/sites/144/2016/02/Animatsiya-na-sajt-menshe-8-mb.gif");
+		$bot->sendMessage($message->getChat()->getId(), $k_txt, "markdown");	
+		$bot->sendMessage($message->getChat()->getId(), $k_txt2, "markdown");	
+
 		}
 		
 	if(mb_stripos($mtext,"Контакты") !== false){
