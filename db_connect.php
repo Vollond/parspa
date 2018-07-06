07458401f2b083cc44d0887d22f7e8529259fdc0
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-06 04:14:04 +0300

    2

--- db_connect.php
+++ db_connect.php
@@ -7,7 +7,7 @@ $username = "bd8f9d12dfabef";
 $databasename = "heroku_6da0db36e924815";
 global $db;
 $db = mysqli_connect($host,$username,$password,$databasename)or die("error: Failed_connect_database");
-mysqli_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
+//mysqli_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
 mysqli_connect('SET NAMES utf8',$db);
 mysqli_connect('SET CHARACTER SET utf8',$db );
 mysqli_connect('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
