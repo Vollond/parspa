030adad385adc7df3ea257a1fdcf4d3a1b09c3a7
Vollond <34167687+Vollond@users.noreply.github.com>
2018-07-06 14:03:36 +0300

    1

--- index.php
+++ index.php
@@ -396,11 +396,11 @@ $bot->on(function($Update) use ($bot){
 	$message = $Update->getMessage();
 	$mtext = $message->getText();
 	$cid = $message->getChat()->getId();
-	
-//if($message->getFrom()->getUsername()==322682583){
+	$uid = $message->getFrom()->getUsername();
+if($uid == 322682583){
 		if($message->getReplyToMessage()->getText() !== false){
 			$bot->sendMessage($message->getReplyToMessage()->getForwardfrom()->getId(), $mtext);
-}//}
+}}
 	
 }, function($message) use ($name){
 	return true; // когда тут true - команда проходит
